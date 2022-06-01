1e4016ec91066e72ac9186bb6e9a09b5deec2133
Thonne Twange <ttwange2@gmail.com>
2022-06-01 22:27:41 +0300

    updated 8-concat_edges.py task solution

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str2 = str[39:67]
+str3 = str[107:111]
+str4 = str[:7]
+str = str2 + str3 + ' '+str4
+print(str)
